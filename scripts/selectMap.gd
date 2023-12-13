e20cf909e1b3154f130ed5f4c5f2b88859add4d8
théau Yapi <theau.yapi@ynov.com>
2023-12-13 17:15:38 +0100

    bientot fini les hud

--- scripts/selectMap.gd
+++ scripts/selectMap.gd
@@ -1,5 +1,19 @@
 extends Panel
 
+@onready var worlds: Array = [$selectMapLayer/MenuMap/mapIcon, $selectMapLayer/MenuMap/mapIcon2, $selectMapLayer/MenuMap/mapIcon3, $selectMapLayer/MenuMap/mapIcon4, $selectMapLayer/MenuMap/mapIcon5, $selectMapLayer/MenuMap/mapIcon6, $selectMapLayer/MenuMap/mapIcon7, $selectMapLayer/MenuMap/mapIcon8, $selectMapLayer/MenuMap/mapIcon9, $selectMapLayer/MenuMap/mapIcon10, $selectMapLayer/MenuMap/mapIcon11, $selectMapLayer/MenuMap/mapIcon12, $selectMapLayer/MenuMap/mapIcon13, $selectMapLayer/MenuMap/mapIcon14, $selectMapLayer/MenuMap/mapIcon15]
+var current_world: int = 0
+
+func _ready():
+	$selectMapLayer/MenuMap/playerIcon.global_position = worlds[current_world].global_position
+	
+func _input(event):
+	if event.is_action_pressed("ui_left") and current_world > 0:
+		current_world -= 1
+		$selectMapLayer/MenuMap/playerIcon.global_position = worlds[current_world].global_position
+	if event.is_action_pressed("ui_right") and current_world < worlds.size() -1:
+		current_world += 1
+		$selectMapLayer/MenuMap/playerIcon.global_position = worlds[current_world].global_position
+
 func _on_select_pressed():
 	# Changez vers la scène de jeu lorsque le bouton "Jouer" est pressé
 # warning-ignore:return_value_discarded
