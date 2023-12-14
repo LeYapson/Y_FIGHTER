404494e527c90bc5067a00da8e58f5b9a204910f
théau Yapi <theau.yapi@ynov.com>
2023-12-14 11:21:25 +0100

    streamdeck upload

--- scenes/ui/map_icon.gd
+++ scenes/ui/map_icon.gd
@@ -1,13 +1,11 @@
 @tool
 extends Control
-@export var world_name: Array = ["Montpellier (FIKA)","Montpellier (Terrasse)","Montpellier (Nouveau Campus)","Toulouse","Bordeaux","Nantes","Rennes","Rouen","Paris","Val d'Europe","Lille","Strasbourg","Lyon","Sofia","Aix"]
 @export var world_index: int = 0
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Label.set_position(Vector2(285,-368))
-	$Label.text = world_name[world_index]
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- scripts/selectMap.gd
+++ scripts/selectMap.gd
@@ -1,17 +1,26 @@
 extends Panel
 
+@export var world_name: Array = ["Montpellier (FIKA)","Montpellier (Terrasse)","New Montpellier","Toulouse","Bordeaux","Nantes","Rennes","Rouen","Paris","Val d'Europe","Lille","Strasbourg","Lyon","Sofia","Aix"]
 @onready var worlds: Array = [$selectMapLayer/MenuMap/mapIcon, $selectMapLayer/MenuMap/mapIcon2, $selectMapLayer/MenuMap/mapIcon3, $selectMapLayer/MenuMap/mapIcon4, $selectMapLayer/MenuMap/mapIcon5, $selectMapLayer/MenuMap/mapIcon6, $selectMapLayer/MenuMap/mapIcon7, $selectMapLayer/MenuMap/mapIcon8, $selectMapLayer/MenuMap/mapIcon9, $selectMapLayer/MenuMap/mapIcon10, $selectMapLayer/MenuMap/mapIcon11, $selectMapLayer/MenuMap/mapIcon12, $selectMapLayer/MenuMap/mapIcon13, $selectMapLayer/MenuMap/mapIcon14, $selectMapLayer/MenuMap/mapIcon15]
 var current_world: int = 0
 
 func _ready():
+	$selectMapLayer/MenuMap/mapName.text = world_name[current_world]
 	$selectMapLayer/MenuMap/playerIcon.global_position = worlds[current_world].global_position
 	
+func _process(delta):
+	if Engine.is_editor_hint():
+		$selectMapLayer/MenuMap/mapName.text = world_name[current_world]
+
+
 func _input(event):
 	if event.is_action_pressed("ui_left") and current_world > 0:
 		current_world -= 1
+		$selectMapLayer/MenuMap/mapName.text = world_name[current_world]
 		$selectMapLayer/MenuMap/playerIcon.global_position = worlds[current_world].global_position
 	if event.is_action_pressed("ui_right") and current_world < worlds.size() -1:
 		current_world += 1
+		$selectMapLayer/MenuMap/mapName.text = world_name[current_world]
 		$selectMapLayer/MenuMap/playerIcon.global_position = worlds[current_world].global_position
 
 func _on_select_pressed():
