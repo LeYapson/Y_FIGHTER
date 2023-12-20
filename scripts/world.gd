596d3370a34a2fe8f6c519d0ada6e56835b82894
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-12-20 16:08:44 +0100

    pause menu

--- scripts/world.gd
+++ scripts/world.gd
@@ -17,7 +17,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_just_pressed("ui_cancel"):
+	if Input.is_action_just_pressed("pause"):
 		pauseMenu()
 		
 	if Input.is_action_just_pressed("left_click"):
